be05da715c2c84c496fec25f84f2dcbcb455c6e4
Lalit <lalits429@gmail.com>
2021-08-20 23:00:40 +0530

    first commit

--- regform.js
+++ regform.js
@@ -1,2 +1,57 @@
-prompt("helloworld");
-console.log("helloworld");
+function clearErrors(){
+
+    errors = document.getElementsByClassName('formerror');
+    for(let item of errors)
+    {
+        item.innerHTML = "";
+    }
+
+
+}
+function seterror(id, error){
+    //sets error inside tag of id 
+    element = document.getElementById(id);
+    element.getElementsByClassName('formerror')[0].innerHTML = error;
+
+}
+
+function validateForm(){
+    var returnval = true;
+    clearErrors();
+
+    //perform validation and if validation fails, set the value of returnval to false
+    var name = document.forms['myForm']["myname"].value;
+    if (name.length<5){
+        seterror("name", "*Length of name is too short");
+        returnval = false;
+    }
+
+    if (name.length == 0){
+        seterror("name", "*Length of name cannot be zero!");
+        returnval = false;
+    }
+
+    var email = document.forms['myForm']["myemail"].value;
+    if (email.length>15){
+        seterror("email", "*Email length is too long");
+        returnval = false;
+    }
+
+    var phone = document.forms['myForm']["mynumber"].value;
+    if (phone.length != 10){
+        seterror("phone", "*Phone number should be of 10 digits!");
+        returnval = false;
+    }
+
+    var address = document.forms['myForm']["myadress"].value;
+    if (address.length < 6){
+
+        
+        seterror("address", "*too small address");
+        returnval = false;
+    }
+
+   
+    return returnval;
+}
+
